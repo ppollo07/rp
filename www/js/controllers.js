19cd6d358c803a1c7c713c57cb79dbaa77916962
ppollo07 <4526553>
2014-03-28 18:04:19 -0300

    Rio Pintos

---
+++ www/js/controllers.js
@@ -0,0 +1,22 @@
+angular.module('starter.controllers', [])
+
+
+// A simple controller that fetches a list of data from a service
+.controller('PetIndexCtrl', function($scope, PetService) {
+  // "Pets" is a service returning mock data (services.js)
+  $scope.pets = PetService.all();
+})
+
+
+// A simple controller that shows a tapped item's data
+.controller('PetDetailCtrl', function($scope, $stateParams, PetService) {
+  // "Pets" is a service returning mock data (services.js)
+  $scope.pet = PetService.get($stateParams.petId);
+})
+
+.controller('SignInCtrl', ['$scope', '$state', function ($scope, $state) {
+	$scope.signIn = function() {
+    	console.log('Sign-In');
+    	$state.go('tab.adopt');
+  	};
+}]);
